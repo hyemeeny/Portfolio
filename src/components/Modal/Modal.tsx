4642274b6d91eef6c7bf7e898469071d1ae02d16
JIHYEMIN <hyemeeeny@gmail.com>
2025-02-06 23:57:40 +0900

    [Style] 자잘한 스타일 수정

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -10,23 +10,25 @@ const Modal = ({ children }: { children: ReactNode }) => {
   if (!isOpen || !project) return null;
 
   return (
-    <div className={s.modalWrap} onClick={closeModal}>
+    <section className={s.modalWrap} onClick={closeModal}>
       <div className={s.modalContent} onClick={(e) => e.stopPropagation()}>
         <button className={s.closeButton} onClick={closeModal}>
           <IoClose />
         </button>
         <div className={s.modalBody}>
-          <div className={s.imageWrap}>
-            <Image src={project.image} fill alt={project.title} />
-          </div>
-          <div className={s.titleWrap}>
-            <h2 className={s.title}>{project.title}</h2>
-            <p className={s.description}>{project.description}</p>
+          <div>
+            <div className={s.titleWrap}>
+              <h2 className={s.mainTitle}>{project.title}</h2>
+              <p className={s.description}>{project.description}</p>
+            </div>
+            <div className={s.imageWrap}>
+              <Image src={project.image} fill alt={project.title} />
+            </div>
           </div>
           {children}
         </div>
       </div>
-    </div>
+    </section>
   );
 };
 
