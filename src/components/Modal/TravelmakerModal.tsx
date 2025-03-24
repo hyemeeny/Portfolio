e8771f7b7824b9ce984817b85f3a08cfd1334469
JIHYEMIN <hyemeeeny@gmail.com>
2025-03-24 20:53:40 +0900

    [Feat] 배포 주소 추가

--- src/components/Modal/TravelmakerModal.tsx
+++ src/components/Modal/TravelmakerModal.tsx
@@ -1,7 +1,9 @@
+import Link from "next/link";
 import Image from "next/image";
 import s from "./Modal.module.scss";
 import { travelmakerTechStack } from "@/data/techStackData";
 import { travelmakerPageData } from "@/data/pageData";
+import { IoLogoGithub } from "react-icons/io";
 
 interface PageProps {
   title: string;
@@ -12,6 +14,18 @@ interface PageProps {
 const TravelmakerModal = () => {
   return (
     <>
+      <div className={s.url}>
+        <h2 className={s.subTitle}>
+          🔗 배포 주소
+          <Link href={"https://github.com/hyemeeny/travelmaker"} target="_blank" className={s.github}>
+            <IoLogoGithub />
+          </Link>
+        </h2>
+        <Link href={"https://travelmaker-ten.vercel.app"} target="_blank" className={s.link}>
+          https://travelmaker-ten.vercel.app/
+        </Link>
+      </div>
+
       <div className={s.techStack}>
         <h2 className={s.subTitle}>⚙️ 기술 스택</h2>
         <ul>
@@ -24,12 +38,12 @@ const TravelmakerModal = () => {
       <div className={s.pageWrap}>
         <h2 className={s.subTitle}>📺 작업 페이지</h2>
         <div className={s.pageList}>
-          {travelmakerPageData.map((page: PageProps, index) => (
-            <div key={index} className={s.pageItem}>
+          {travelmakerPageData.map((page: PageProps) => (
+            <div key={page.title} className={s.pageItem}>
               <h3 className={s.pageTitle}>{page.title}</h3>
               <ul className={s.skillDescription}>
-                {page.description.map((desc, index) => (
-                  <li key={index}>{desc}</li>
+                {page.description.map((desc) => (
+                  <li key={desc}>{desc}</li>
                 ))}
               </ul>
               {page.image && <Image src={page.image} width={800} height={100} alt={`${page.title} image`} />}
