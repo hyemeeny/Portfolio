0ad376c7d56ba0157489ab2e3176b88464aec43a
JIHYEMIN <hyemeeeny@gmail.com>
2025-03-27 21:13:15 +0900

    [Feat] Contact 페이지 추가

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,9 +1,76 @@
+import Link from "next/link";
+import s from "./Contact.module.scss";
 import Container from "@/components/Layout/Container";
 
 const Contact = () => {
   return (
-    <section>
-      <Container>Contact Page</Container>
+    <section className={s.contactPage}>
+      <Container>
+        <h2 className={s.title}>Contact with me! 🤙</h2>
+        <div className={s.contactLine}>
+          <div className={s.number}>
+            <p>1</p>
+            <p>2</p>
+            <p>3</p>
+            <p>4</p>
+            <p>5</p>
+            <p>6</p>
+            <p>7</p>
+            <p>8</p>
+            <p>9</p>
+            <p>10</p>
+            <p>11</p>
+          </div>
+          <div className={s.text}>
+            <p>
+              <span>.Contact</span> &#123;
+            </p>
+            <div className={s.contactWrap}>
+              <div className={s.contactbox}>
+                <p>
+                  <span>.Channel</span> &#123;
+                </p>
+                <div className={s.info}>
+                  <p>
+                    Email :
+                    <Link href={"mailto:hyemeeeny@gmail.com"} target="_blank" rel="noopener noreferrer">
+                      <span>hyemeeeny@gmail.com</span>
+                    </Link>
+                  </p>
+                  <p>
+                    Phone :
+                    <Link href={"tel:+821037181373"} target="_blank" rel="noopener noreferrer">
+                      <span>010-3718-1373</span>
+                    </Link>
+                  </p>
+                </div>
+                <p>&#125;</p>
+              </div>
+              <div className={s.contactbox}>
+                <p>
+                  <span>.Social</span> &#123;
+                </p>
+                <div className={s.info}>
+                  <p>
+                    GitHub :
+                    <Link href={"https://github.com/hyemeeny"} target="_blank" rel="noopener noreferrer">
+                      <span>https://github.com/hyemeeny</span>
+                    </Link>
+                  </p>
+                  <p>
+                    Blog :
+                    <Link href={"https://velog.io/@hyemeeny"} target="_blank" rel="noopener noreferrer">
+                      <span>https://velog.io/@hyemeeny</span>
+                    </Link>
+                  </p>
+                </div>
+                <p>&#125;</p>
+              </div>
+            </div>
+            <p>&#125;</p>
+          </div>
+        </div>
+      </Container>
     </section>
   );
 };
