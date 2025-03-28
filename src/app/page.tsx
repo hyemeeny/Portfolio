4b79b2862c3822632d9cf18b95cc787c037d5f0a
JIHYEMIN <hyemeeeny@gmail.com>
2025-03-28 20:45:23 +0900

    [Chore] 문구 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,34 +4,37 @@ import Link from "next/link";
 import s from "@/styles/Home.module.scss";
 import { Typewriter } from "react-simple-typewriter";
 import { VscVscode } from "react-icons/vsc";
+import Container from "@/components/Layout/Container";
 import Button from "@/components/Button/Button";
 
 const Home = () => {
   return (
-    <main className={s.mainPage}>
-      <VscVscode className={s.vsCodeImg} />
-      <div className={s.homeTitleWrap}>
-        <h1 className={s.homeTitle}>
-          <Typewriter
-            words={["Hello👋 Hyemin’s Portfolio!", "I'm Frontend Developer"]}
-            loop={5}
-            cursor
-            cursorStyle="|"
-            typeSpeed={70}
-            deleteSpeed={50}
-            delaySpeed={1000}
-          />
-        </h1>
-        <div className={s.buttonWrap}>
-          <Button type="fill">
-            <Link href="/project">View Works</Link>
-          </Button>
-          <Button type="empty">
-            <Link href="/about">About me</Link>
-          </Button>
+    <Container>
+      <main className={s.mainPage}>
+        <VscVscode className={s.vsCodeImg} />
+        <div className={s.homeTitleWrap}>
+          <h1 className={s.homeTitle}>
+            <Typewriter
+              words={["Hello, Hyemin’s Portfolio!", "I'm Frontend Developer"]}
+              loop={5}
+              cursor
+              cursorStyle="|"
+              typeSpeed={70}
+              deleteSpeed={50}
+              delaySpeed={1000}
+            />
+          </h1>
+          <div className={s.buttonWrap}>
+            <Button type="fill">
+              <Link href="/project">View Works</Link>
+            </Button>
+            <Button type="empty">
+              <Link href="/about">About me</Link>
+            </Button>
+          </div>
         </div>
-      </div>
-    </main>
+      </main>
+    </Container>
   );
 };
 
