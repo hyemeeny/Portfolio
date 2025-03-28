b06248035e159ad5270c2c6544c5fa0c588b977e
JIHYEMIN <hyemeeeny@gmail.com>
2025-03-28 21:55:13 +0900

    [Chore] 문구 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,7 +14,15 @@ const Home = () => {
         <VscVscode className={s.vsCodeImg} />
         <div className={s.homeTitleWrap}>
           <h1 className={s.homeTitle}>
-            <Typewriter words={["Hello👋 Hyemin's world!"]} loop={5} cursor cursorStyle="|" typeSpeed={70} deleteSpeed={50} delaySpeed={1000} />
+            <Typewriter
+              words={["Hello👋 Hyemin's world!", "Welcome Hyemin's portfolio!"]}
+              loop={5}
+              cursor
+              cursorStyle="|"
+              typeSpeed={70}
+              deleteSpeed={50}
+              delaySpeed={1000}
+            />
           </h1>
           <p className={s.homeSubTitle}>I&apos;m Frontend Developer</p>
           <div className={s.buttonWrap}>
