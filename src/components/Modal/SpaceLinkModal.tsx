22079d7931a48dca260e2a42ae867b9b6f7f2b9d
지혜민 <hyemeeeny@gmail.com>
2026-03-10 18:55:40 +0900

    [Chore] 타입 공통 컴포넌트로 수정

--- src/components/Modal/SpaceLinkModal.tsx
+++ src/components/Modal/SpaceLinkModal.tsx
@@ -1,15 +1,10 @@
-import Link from "next/link";
-import Image from "next/image";
-import s from "./Modal.module.scss";
-import { spaceLinkTechStack } from "@/data/techStackData";
-import { spaceLinkPageData } from "@/data/pageData";
-import { VscGithubInverted } from "react-icons/vsc";
-
-interface PageProps {
-  title: string;
-  description: string[];
-  image?: string;
-}
+import Link from 'next/link';
+import Image from 'next/image';
+import s from './Modal.module.scss';
+import { ModalPageProps } from '@/types/modal';
+import { spaceLinkTechStack } from '@/data/techStackData';
+import { spaceLinkPageData } from '@/data/pageData';
+import { VscGithubInverted } from 'react-icons/vsc';
 
 const SpaceLinkModal = () => {
   return (
@@ -17,17 +12,17 @@ const SpaceLinkModal = () => {
       <div className={s.url}>
         <h2 className={s.subTitle}>
           🔗 배포 주소
-          <Link href={"https://github.com/hyemeeny/SpaceLink"} target="_blank" className={s.github} aria-label="GitHub 저장소로 이동">
-            <VscGithubInverted aria-hidden="true" />
+          <Link href={'https://github.com/hyemeeny/SpaceLink'} target='_blank' className={s.github} aria-label='GitHub 저장소로 이동'>
+            <VscGithubInverted aria-hidden='true' />
           </Link>
         </h2>
-        <Link href={"https://spacelink-hyemin.vercel.app"} target="_blank" className={s.link} aria-label="SpaceLink 배포 사이트 열기">
+        <Link href={'https://spacelink-hyemin.vercel.app'} target='_blank' className={s.link} aria-label='SpaceLink 배포 사이트 열기'>
           https://spacelink-hyemin.vercel.app
         </Link>
       </div>
 
       <div className={s.techStack}>
-        <h2 id="tech-stack" className={s.subTitle}>
+        <h2 id='tech-stack' className={s.subTitle}>
           ⚙️ 기술 스택
         </h2>
         <ul>
@@ -38,11 +33,11 @@ const SpaceLinkModal = () => {
       </div>
 
       <div className={s.pageWrap}>
-        <h2 id="pages" className={s.subTitle}>
+        <h2 id='pages' className={s.subTitle}>
           📺 작업 페이지
         </h2>
         <div className={s.pageList}>
-          {spaceLinkPageData.map((page: PageProps) => (
+          {spaceLinkPageData.map((page: ModalPageProps) => (
             <article key={page.title} className={s.pageItem} aria-labelledby={`page-${page.title}`}>
               <h3 className={s.pageTitle}>{page.title}</h3>
               <ul className={s.skillDescription}>
